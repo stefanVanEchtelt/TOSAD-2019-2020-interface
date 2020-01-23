c4b32c4d07dd4d4efa3d95c09355f9263acd5e5d
JobStoker <job.stoker@gmail.com>
2020-01-23 11:59:58 +0100

    x

--- script/rule.js
+++ script/rule.js
@@ -2,12 +2,12 @@ const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
 
 $(document).ready(function() {
-    let tableName = urlParams.get('table');
-    let columnName = urlParams.get('column');
+    var tableName = urlParams.get('table');
+    var columnName = urlParams.get('column');
 
-    //$("#form_table").val(tableName);
-    //$("#form_column").val(columName);
-    //$("#head_name").innerHTML(columnName);
+    $("#form_table").val(tableName);
+    $("#form_column").val(columnName);
+    $("#head_name").html(columnName);
 
     fetch('http://localhost:8080/api/tosad/table', {method: 'GET'})
     .then((response) => {
@@ -24,27 +24,100 @@ $(document).ready(function() {
         }
     });
 
-    $("#table1").change(function(){
-        getColumnsByTable($("#table1").val());
-        console.log("changed");
+    $("#table2").change(function(){
+        getColumnsByTable($("#table2").val(), $("#column2"));
+    });
+
+    $("#form_rule").change(function(){
+        if($("#form_rule").val() == "TCMP"){
+            getColumnsByTable(tableName, $("#column1"));
+            getColumnsByTable(tableName, $("#column2"));    
+        }        
     });
 });
 
-function getColumnsByTable(table){
+function getColumnsByTable(table, column){
     fetch('http://localhost:8080/api/tosad/column/' + table, {method: 'GET'})
         .then((response) => {
             if (response.ok) {
                 return response.json();
             }
         }).then((columns) => {
-        console.log(columns);    
-        let column1 = $("#column1");
-        let column2 = $("#column2");   
-        column1.empty();
+        column.empty();
         let i;
         for (i = 0; i < columns.length; i++) {
-            column1.append($("<option value=" + columns[i].name + ">" + columns[i].name + "</option>"));
-            column2.append($("<option value=" + columns[i].name + ">" + columns[i].name + "</option>"));  
+            column.append($("<option value=" + columns[i].name + ">" + columns[i].name + "</option>"));
         }
     });
 }
+
+//var rule_name = $("#form_rule_name");
+initForm();
+
+var rule = $("#form_rule");
+$("#form_save").click(sendRule);
+
+rule.change(function(){
+    initForm();
+
+    switch(rule.val()){
+        case "ARNG":
+            $("#form_group_value").show();
+            $("#form_group_value_extra").show();
+            $("#form_group_operator_arng").show();
+            break;
+        case "ACMP":
+            $("#form_group_operator").show();
+            $("#form_group_value").show();
+            break;
+        case "ALIS":
+            $("#form_group_list").show();
+            break;
+        case "TCMP":
+            $("#form_group_column").show();
+            $("#form_group_column2").show();    
+            $("#form_group_operator").show();                   
+            break;
+        case "ICMP":
+            $("#form_group_table2").show();
+            $("#form_group_column2").show();        
+            $("#form_group_operator").show();               
+    }
+});
+
+function initForm(){
+    $("#form_group_operator").hide();
+    $("#form_group_operator_arng").hide();
+    $("#form_group_table").hide();
+    $("#form_group_table2").hide();
+    $("#form_group_column").hide();
+    $("#form_group_column2").hide();    
+    $("#form_group_compare").hide();
+    $("#form_group_value").hide();
+    $("#form_group_value_extra").hide();    
+    $("#form_group_list").hide();
+}
+
+function sendRule(){
+    let formData = new FormData(document.querySelector("#generate_rule"));
+    let encData = new URLSearchParams(formData.entries());
+
+    for (var pair of formData.entries()) {
+    console.log(pair[0]+ ', ' + pair[1]); 
+    }
+    
+ /*  fetch('http://localhost:8080/api/tosad/businessRule/businessRule', {method: 'POST', body: encData})
+    .then((response) => { 
+        if (response.ok) {
+            console.log('succes 1');
+            console.log(response.json());
+            console.log('succes 2');
+        } 
+        else {
+            console.log(response.json());
+        }
+    })
+    .then((myJson) => {
+    });  */
+}
+
