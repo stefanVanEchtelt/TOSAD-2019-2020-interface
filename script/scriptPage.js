6997911febbf28b707c92564ecd56469759be94a
JelleStoof <jellestoof@gmail.com>
2020-01-23 15:30:26 +0100

    execute done

--- script/scriptPage.js
+++ script/scriptPage.js
@@ -1,5 +1,7 @@
 getBusinessRules();
 
+
+
 function getBusinessRules() {
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
@@ -13,16 +15,41 @@ function getBusinessRules() {
             }
         })
         .then((ruleJson) => {
-            let py5 = document.getElementById('py-5-id');
             let index;
             for (index = 0; index < ruleJson.length; index++){
-                py5.innerHTML += getCardHtml(ruleJson[index].id);
+                if (ruleJson[index].isExecuted === 1) {
+                    } else {
+                    let code = fetchCode(ruleJson[index].id);
+                }
             }
+            $("#"+3).click(function(){handleRun("ooooi")});
+
             console.log("then myJson");
+
+        })
+
+}
+
+function fetchCode(ruleId) {
+    let py5 = document.getElementById('py-5-id');
+    let code;
+    fetch('http://localhost:8080/api/tosad/businessRule/rules/businessRules/example/'+ruleId, {method: 'GET'})
+        .then((response) => {
+            if(response.ok) {
+                return response.json();
+            } else {
+                console.log('fetch failed');
+            }
+        })
+        .then((codeJson) => {
+            code = codeJson;
+            py5.innerHTML += getCardHtml(code, ruleId);
+
         })
 }
 
-function getCardHtml(ruleName) {
+
+function getCardHtml(ruleName, ruleId) {
     let html =
         "<div class=\"container\">\n" +
         "      <div class=\"row\">\n" +
@@ -30,13 +57,32 @@ function getCardHtml(ruleName) {
         "          <div class=\"card\">\n" +
         "            <div class=\"card-body\">\n" +
         "              <p class=\"card-text\">" +
+        "                   <pre>    "+
                             ruleName +
+        "                   </pre>" +
         "              </p>" +
-        "               <a class=\"btn btn-info mr-3\" href=\"#\" style=\"\">Run</a><a class=\"btn btn-info\" href=\"table.html\">Terug</a>\n" +
+        "               <a onclick=handleRun("+ ruleId+ ") id=" + ruleId + " class=\"btn btn-info mr-3\" style=\"\">Run</a><a class=\"btn btn-info\" href=\"table.html\">Terug</a>\n" +
         "            </div>\n" +
         "          </div>\n" +
         "        </div>\n" +
         "      </div>\n" +
         "    </div>";
+
     return html;
+}
+
+function handleRun(ruleId){
+    console.log(ruleId);
+    fetch('http://localhost:8080/api/tosad/businessRule/rules/businessRules/execute/'+ruleId, {method: 'POST'})
+        .then((response) => {
+            if(response.ok) {
+                return response.json();
+            } else {
+                console.log('fetch failed');
+            }
+        })
+        .then((codeJson) => {
+            console.log(codeJson);
+
+        })
 }
