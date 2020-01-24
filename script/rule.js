bd520a0e0b62db7052e2899eac1c7b76d18181c0
JobStoker <job.stoker@gmail.com>
2020-01-24 11:02:37 +0100

    form

--- script/rule.js
+++ script/rule.js
@@ -105,12 +105,12 @@ function sendRule(){
    fetch('http://localhost:8080/api/tosad/businessRule/businessRule', {method: 'POST', body: encData})
     .then((response) => { 
         if (response.ok) {
-            console.log('succes 1');
             console.log(response.json());
-            console.log('succes 2');
+            window.location.replace("table.html");
         } 
         else {
             console.log(response.json());
+            alert(response.json());
         }
     })
     .then((myJson) => {
